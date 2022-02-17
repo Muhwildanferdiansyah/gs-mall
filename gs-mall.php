e4e2442b8504881492ee1b1e75f8f71ca48b7f36
Muhwildanferdiansyah <73091498+Muhwildanferdiansyah@users.noreply.github.com>
2022-02-17 21:36:57 +0700

    update checker ready

--- gs-mall.php
+++ gs-mall.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Griya Samara Mall
  * Plugin URI:        http://gsmall.id
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            YukDiOrder Agency
  * Author URI:        http://yukdiorder.com
  * License:           GPL-2.0+
